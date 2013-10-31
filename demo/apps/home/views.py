4981074fdd81d2948eede754a2c10a368c4b9861
franklin <franpad8@gmail.com>
2013-10-31 12:55:20 -0400

    Se muestran las categorias de los productos en la pagina singular de estos

--- demo/apps/home/views.py
+++ demo/apps/home/views.py
@@ -33,7 +33,8 @@ def productos_view(request, pagina):
 
 def singleProducto_view(request, id_prod):
 	prod = producto.objects.get(id=id_prod)
-	ctx = {'producto': prod}
+	cats = prod.categoria.all()
+	ctx = {'producto': prod, 'categorias':cats}
 	return render_to_response('home/SingleProducto.html', ctx, context_instance=RequestContext(request))	
 
 def contacto_view(request):
